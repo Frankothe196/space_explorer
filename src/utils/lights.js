b6638a373a7d49cb490aad88cf9e37f4231bbf3f
Franklin Barto <kipkalisbartoo@gmail.com>
2022-05-16 17:03:18 +0300

    Textures loaded successfully

---
+++ src/utils/lights.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three'
+import worldSetup from './worldSetup'
+
+class Lighting {
+    constructor(){
+        this.world = new worldSetup()
+        this.scene = this.world.scene
+        this.setLights()
+    }
+    setLights(){
+        this.lights ={}
+        this.lights.ambientLight = new THREE.AmbientLight( 0xffffff, 1 );
+        this.scene.add(this.lights.ambientLight)
+
+        this.lights.pointLight = new THREE.PointLight(0xffffff,1,150)
+        this.scene.add(this.lights.pointLight)
+        
+        this.lights.pointLight.position.set(0,100,0)
+        this.lights.pointLightHelper = new THREE.PointLightHelper(this.lights.pointLight,1)
+        this.scene.add(this.lights.pointLightHelper)
+
+        
+    }
+}
+
+export {Lighting}
